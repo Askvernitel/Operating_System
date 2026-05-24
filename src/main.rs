515bb3a407eb22640aa33851e5f337cd67bf569e
Askvernitel <danielkolotashvilio@gmail.com>
2026-05-24 20:50:22 +0400

    init(OS): start os development

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
@@ -7,6 +8,16 @@ fn panic(_info: &PanicInfo) -> !{
     loop{}
 }
 
-fn main() {
 
+static HELLO: &[u8] = b"Hello World!";
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> !{
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate(){
+        unsafe{
+            *vga_buffer.offset(i as isize)
+        }
+    }
+    loop{ }
 }
