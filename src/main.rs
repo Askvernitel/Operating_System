43e5732e809a7fc419451d8d2f0059fc81dee8e4
Askvernitel <danielkolotashvilio@gmail.com>
2026-05-31 18:24:58 +0400

    feat(tests): add test execution

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 
 use core::panic::PanicInfo;
 use crate::vga_buffer::{Color, ColorCode, Writer };
@@ -12,10 +16,21 @@ fn panic(_info: &PanicInfo) -> !{
     loop{}
 }
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]){
+    println!("Running {} Tests Yippie", tests.len());
+
+    for test in tests{ 
+        test();
+    }
+}
 
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> !{
+
+    #[cfg(test)]
+    test_main();
 
     println!("Hello World\n cool coolc oocl {}", 2);
     panic!("Help");
