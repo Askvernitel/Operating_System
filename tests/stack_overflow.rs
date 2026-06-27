03e6325db9a681b9a9d3f3cc322f4ccbed8407a9
Askvernitel <danielkolotashvilio@gmail.com>
2026-06-27 12:21:15 +0400

    feat(interrupt): add pic

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -1,26 +1,47 @@
 #![no_std]
 #![no_main]
-
+#![feature(abi_x86_interrupt)]
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use lazy_static::lazy_static;
 use core::panic::PanicInfo;
-use Operating_System::gdt::init;
+use Operating_System::{QemuExitCode, exit_qemu, gdt::{DOUBLE_FAULT_IST_INDEX, init}, serial_print, serial_println};
 
+lazy_static!{
+    pub static ref TEST_IDT:InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe{
+            idt.double_fault.set_handler_fn(test_double_fault_handler)
+                .set_stack_index(DOUBLE_FAULT_IST_INDEX);
+        }
+        idt
+    };
+}
 
+extern "x86-interrupt" fn test_double_fault_handler(_stack_frame:InterruptStackFrame, _error_code:u64) -> !{
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::SUCCESS);
+    loop{}
+}
 
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> !{
+    serial_print!("stack_overflow test...\t");
     init();
+    init_test_idt();
     stack_overflow();
-    loop{}
+
+    panic!("stack_overflow test continued");
 }
 
 #[allow(unconditional_recursion)]
 fn stack_overflow(){ 
     stack_overflow();
-    volatile::Volatile::new(0).read();
 }
 
-
+fn init_test_idt(){
+    TEST_IDT.load();
+}
 
 #[panic_handler]
 fn panic(_info:&PanicInfo) -> !{
